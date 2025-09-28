4bb8a9bb1efb6bc4b6769e568afbe22381f17007
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-09-29 01:10:51 +0330

    importing

---
+++ drilling_processor/pipelines/ml_pipeline.py
@@ -0,0 +1,5 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
